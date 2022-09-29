b6694bcc7b80591377dab6ff4a03ef3722cadf21
JesperWintberg <111962418+JesperWintberg@users.noreply.github.com>
2022-09-29 19:50:28 +0200

    Add project files.

---
+++ Kappitel 3 uppgift-3.15/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra en bil?");
+            string tim = Console.ReadLine();
+
+            float tid = float.Parse(tim);
+
+            float tide = (tid * 80);
+
+            if (tide < 950)
+            {
+                Console.WriteLine("Bilen kommer att kosta dig " + tide);
+            }
+
+            else
+            {
+                Console.WriteLine("Du får tyvärr inte spendera mer en 950kr på din bil");
+            }
+
+        }
+    }
+}
+
